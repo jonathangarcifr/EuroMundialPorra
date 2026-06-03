318c43114fe0b12c0c6dfcdd67f207d09d681593
Jonathan Garcia Fraile <jonathangarcifr@gmail.com>
2026-06-04 01:36:35 +0200

    Añade páginas de error amigables

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+&jt79*cdb97%9lsevibw++_6m(vx!d76ch04p*qn&0498-b3g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "apuestas" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
